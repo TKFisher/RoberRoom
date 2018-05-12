a8d8526871ad33dc6f3a192ceb4e5764619cbfbc
Haile Asnake <hasnake@sbgtv.com>
2018-05-12 15:43:59 -0700

    Adding js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var dt = new Date();  
 
 // Display the month, day, and year. getMonth() returns a 0-based number.  
-var month = dt.getMonth()+1;  
-var day = dt.getDate();  
-var year = dt.getFullYear();  
-document.write(month + '-' + day + '-' + year);  
+var hour = dt.getHours();  
+var minutes = dt.getMinutes();  
+var seconds = dt.getSeconds();  
+document.write(hour + '-' + minutes + '-' + seconds+'-');  
 
 // Output: current month, day, year  
